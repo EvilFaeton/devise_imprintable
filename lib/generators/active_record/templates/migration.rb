ae6a6cfdb9e3bd8a3b28852826a0c57fb624a620
Gal Steinitz <gal@steinitz.com>
2013-02-09 22:48:42 -0800

    Update lib/generators/active_record/templates/migration.rb
    Fixed typo in migration generator

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseInvitableAddTo<%= table_name.camelize %> < ActiveRecord::Migration
+class DeviseImprintableAddTo<%= table_name.camelize %> < ActiveRecord::Migration
   def up
     change_table :<%= table_name %> do |t|
       t.datetime   :first_sign_in_at
